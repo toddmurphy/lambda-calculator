7f4e4b54de5b8deaccc0e2514ac8f741241c8f16
toddmurphy <toddmurphy93@gmail.com>
2019-11-05 21:56:53 -0400

    updated Specials file to code right before working on SpecialButton in case error happens again

--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -10,9 +10,18 @@ const Specials = () => {
   // STEP 2 - add the imported data to state
   const [characterButtons, setCharacterButtons] = useState(specials);
 
-  return <div>{/* STEP 3 - Use .map() to iterate over your array data and return a button
-       component matching the name on the provided file. Pass
-       it any props needed by the child component*/}</div>;
+  return (
+    //map 'characterButtons' and output  return 'SpecialButton' buttons.Pass it any props needed by the child component
+    characterButtons.map((character, index) => {
+      return <SpecialButton key={index} character={character} />;
+    })
+  );
 };
 
 export default Specials;
+
+{
+  /* STEP 3 - Use .map() to iterate over your array data and return a button
+       component matching the name on the provided file. Pass
+       it any props needed by the child component*/
+}
